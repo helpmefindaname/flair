3eea9bc52e1c47a7fd58a219e5ea94769e9e61a6
Benedikt Fuchs <benedikt.fuchs.staw@hotmail.com>
2025-02-14 19:55:48 +0100

    random changes

--- flair/models/multitask_model.py
+++ flair/models/multitask_model.py
@@ -15,7 +15,7 @@ from flair.training_utils import Result
 log = logging.getLogger("flair")
 
 
-class MultitaskModel(flair.nn.Classifier):
+class MultitaskModel(flair.nn.Model):
     """Multitask Model class which acts as wrapper for creating custom multitask models.
 
     Takes different tasks as input, parameter sharing is done by objects in flair,
@@ -27,7 +27,7 @@ class MultitaskModel(flair.nn.Classifier):
 
     def __init__(
         self,
-        models: list[flair.nn.Classifier],
+        models: list[flair.nn.Model],
         task_ids: Optional[list[str]] = None,
         loss_factors: Optional[list[float]] = None,
         use_all_tasks: bool = False,
@@ -44,7 +44,7 @@ class MultitaskModel(flair.nn.Classifier):
 
         task_ids_internal: list[str] = task_ids if task_ids else [f"Task_{i}" for i in range(len(models))]
 
-        self.tasks: dict[str, flair.nn.Classifier] = {}
+        self.tasks: dict[str, flair.nn.Model] = {}
         self.loss_factors: dict[str, float] = {}
         self.use_all_tasks = use_all_tasks
 
